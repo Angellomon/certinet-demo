08b815172f01c6f950e1dbcec0e6e1c7dce1774e
Angel M <angelmtzdiaz@gmail.com>
2025-03-06 14:51:35 -0600

    style(routes): impl style for profesionista's login

--- src/routes/login/profesionista/+page.svelte
+++ src/routes/login/profesionista/+page.svelte
@@ -1,3 +1,20 @@
-<main>
+<script lang="ts">
+	import EmailInput from "../email-input.svelte";
+	import PasswordInput from "../password-input.svelte";
+
+	let email = $state("")
+	let password = $state("")
+
+</script>
+
+<main class="flex flex-col items-center gap-10">
 	<h1 class="text-3xl">Login Profesionista</h1>
+
+	<div class="flex flex-col gap-3 items-center max-w-1/2">
+		<EmailInput bind:email />
+
+		<PasswordInput bind:password />
+
+		<button class="btn btn-primary inline self-end" onclick={() => console.log(email)}>Login</button>
+	</div>
 </main>
---
+++ src/routes/profesionistas/+page.svelte
@@ -0,0 +1,3 @@
+<main>
+  <h1 class="text-4xl">Panel Profesionista</h1>
+</main>
