310741ddd86ad3601ee46d8192a5453631614df5
Angel M <angelmtzdiaz@gmail.com>
2025-03-06 00:54:24 -0600

    feat(routes): impl init Login

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,4 +1,17 @@
-<nav class="hidden lg:flex lg:flex-row lg:justify-end relative items-center">
-  <div class="absolute left-5">CertiNet Logo</div>
-  <div>Navs</div>
+<script lang="ts">
+	const paths = [
+		{
+			path: '/login',
+			name: 'Login'
+		}
+	];
+</script>
+
+<nav class="relative hidden items-center lg:flex lg:flex-row lg:justify-end lg:px-5 gap-5">
+	<div class="absolute left-5">CertiNet Logo</div>
+	<ul>
+    {#each paths as path}
+      <li><a href={path.path}>{path.name}</a></li> 
+    {/each}
+  </ul>
 </nav>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,3 @@
+<main>
+	<h1 class="text-3xl">Login</h1>
+</main>
