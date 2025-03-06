b54f7e9a339983d1b58bfe679301417437964962
Angel M <angelmtzdiaz@gmail.com>
2025-03-06 01:05:23 -0600

    feat(routes): impl init login for profesionista & empleador

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,3 +1,7 @@
 <main>
 	<h1 class="text-3xl">Login</h1>
+  <div class="flex flex-row gap-10 justify-center">
+    <a class="btn btn-primary" href="/login/profesionista">Soy Profesionista</a>
+    <a class="btn btn-secondary" href="/login/empleador">Soy Empleador</a>
+  </div>
 </main>
---
+++ src/routes/login/empleador/+page.svelte
@@ -0,0 +1,3 @@
+<main>
+	<h1 class="text-3xl">Login Empleador</h1>
+</main>
