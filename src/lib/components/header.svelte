26d800e236daf501e7ae15dda6a94320fed096fe
Angel M <angelmtzdiaz@gmail.com>
2025-03-07 12:38:09 -0600

    style: link class

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -15,7 +15,7 @@
 	<a class="absolute left-5 py-3 px-5" href="/"> <div class="text-3xl">CertiNet Logo</div></a>
 	<ul class="flex flex-row gap-5">
 		{#each paths as path}
-			<li><a href={path.path}>{path.name}</a></li>
+			<li><a class="link-hover" href={path.path}>{path.name}</a></li>
 		{/each}
 	</ul>
 </nav>
