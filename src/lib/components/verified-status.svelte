b2f1e1b1a85f71b9439fd911cad2da978e3fe6bf
Angel M <angelmtzdiaz@gmail.com>
2025-03-10 17:00:41 -0600

    feat(components): impl status component

---
+++ src/lib/components/verified-status.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	const { text = '', verified = false } = $props();
+</script>
+
+<div class="flex flex-row justify-center items-center gap-1">
+  {#if verified}
+	<div class="inline-grid *:[grid-area:1/1]">
+		<div class="status status-accent"></div>
+		<div class="status status-accent"></div>
+	</div>
+{:else}
+	<div class="inline-grid *:[grid-area:1/1]">
+		<div class="status status-error animate-ping"></div>
+		<div class="status status-error"></div>
+	</div>
+{/if}
+
+{text}
+
+</div>
