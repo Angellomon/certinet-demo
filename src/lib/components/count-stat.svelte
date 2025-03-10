5e980c08086c90e512848e88335a0c59251d0990
Angel M <angelmtzdiaz@gmail.com>
2025-03-10 16:59:32 -0600

    feat(components): impl Count component

---
+++ src/lib/components/count-stat.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { formatNumber } from '$lib/utils';
+
+	const { count = 0, description = '', title = 'Total' } = $props();
+</script>
+
+<div class="stats shadow">
+	<div class="stat">
+		<div class="stat-title">{title}</div>
+		<div class="stat-value">{formatNumber(count)}</div>
+
+		{#if description}
+			<div class="stat-desc">{description}</div>
+		{/if}
+	</div>
+</div>
