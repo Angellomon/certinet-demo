70a01e41242aeebe9a9f762b1d0d1cfa895d4dfa
Angel M <angelmtzdiaz@gmail.com>
2025-03-11 13:48:27 -0600

    feat(routes): impl profesionista's certificates routes

---
+++ src/routes/profesionista/certificaciones/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import ListaCertificaciones from '$lib/components/lista-certificaciones.svelte';
+
+	const { data } = $props();
+	const { certificaciones } = data;
+</script>
+
+<main class="flex flex-col gap-5">
+	<div class="breadcrumbs text-sm">
+		<ul>
+			<li><a href="/profesionista">Dashboard</a></li>
+			<li><a href="/profesionista/certificaciones">Certificaciones</a></li>
+		</ul>
+	</div>
+
+	<h2 class="text-2xl">Certificaciones</h2>
+
+	<ListaCertificaciones {certificaciones} />
+</main>
