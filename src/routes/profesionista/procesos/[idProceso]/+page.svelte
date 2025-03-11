380444a6e8df30cc1a98e9332b9181c04be4eb87
Angel M <angelmtzdiaz@gmail.com>
2025-03-11 13:49:18 -0600

    feat(routes): impl procesos routes

---
+++ src/routes/profesionista/procesos/[idProceso]/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import JsonData from '$lib/components/JsonData.svelte';
+
+	const { data } = $props();
+	const { proceso } = data;
+</script>
+
+<main>
+	<div class="breadcrumbs text-sm">
+		<ul>
+			<li><a href="/profesionista">Dashboard</a></li>
+			<li><a href="/profesionista/procesos">Procesos</a></li>
+		</ul>
+		
+		<JsonData title="Raw Data Proceso" jsonData={proceso} />
+	</div>
+</main>
