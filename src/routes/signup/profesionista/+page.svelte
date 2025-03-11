306cbef90ad7ff9f0e4299054e49cfb9663593d3
Angel M <angelmtzdiaz@gmail.com>
2025-03-10 18:14:54 -0600

    feat(routes): impl profesionista signup

--- src/routes/signup/profesionista/+page.svelte
+++ src/routes/signup/profesionista/+page.svelte
@@ -1,3 +1,32 @@
-<main>
+<main class="flex flex-col gap-5 justify-center items-center">
 	<h1 class="text-3xl">Signup Profesionista</h1>
+
+	<form class="flex flex-col px-5 gap-1">
+
+		<label class="input">
+			<span class="label">Nombre</span>
+			<input class="input" name="nombre" id="nombre" type="text"  />
+		</label>
+
+		<label class="input">
+			<span class="label">Apellidos</span>
+			<input class="input" name="nombre" id="nombre" type="text"/>
+		</label>
+
+		<label class="input">
+			<span class="label">
+				Fecha de Nacimiento
+			</span>
+			<input class="input" type="date" name="fechaNacimiento" id="fechaNacimiento" placeholder="Fecha de Nacimiento">
+		</label>
+
+		<label class="input">
+			<span class="label">Profesión</span>
+			<input class="input" type="text" name="profesion" id="profesion" placeholder="Profesión">
+		</label>
+
+		<a href="/profesionista" >
+			<button class="btn btn-secondary">Signup</button>
+		</a>
+	</form>
 </main>
