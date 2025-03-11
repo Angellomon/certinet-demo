e24edd64e8e553a49be2631612eff1246f7c6d41
Angel M <angelmtzdiaz@gmail.com>
2025-03-10 18:48:01 -0600

    style: better header

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -11,11 +11,16 @@
 	];
 </script>
 
-<nav class="hidden lg:relative lg:gap-5 lg:flex lg:flex-row lg:justify-end lg:align-middle lg:items-center lg:px-5 lg:py-5">
-	<a class="absolute left-5 py-3 px-5" href="/"> <div class="text-3xl">CertiNet Logo</div></a>
-	<ul class="flex flex-row gap-5">
-		{#each paths as path}
-			<li><a class="link-hover" href={path.path}>{path.name}</a></li>
-		{/each}
-	</ul>
+<nav class="navbar bg-base-100 shadow-sm">
+	<div class="flex-1">
+		<a href="/" class="btn btn-ghost text-xl">Certinet Demo</a>
+	</div>
+
+	<div class="flex-none">
+		<ul class="menu menu-horizontal px-1">
+			{#each paths as path}
+				<li><a href={path.path}>{path.name}</a></li>
+			{/each}
+		</ul>
+	</div>
 </nav>
