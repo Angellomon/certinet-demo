94c56114f1571bba9c0e96e215a44f5c34941627
Angel M <angelmtzdiaz@gmail.com>
2025-03-12 02:31:26 -0600

    feat(styling)

--- src/lib/components/lista-certificaciones.svelte
+++ src/lib/components/lista-certificaciones.svelte
@@ -3,7 +3,7 @@
 	import CountStat from './count-stat.svelte';
 	import VerifiedStatus from './verified-status.svelte';
 
-	const { certificaciones } = $props<{ certificaciones: Certificacion[] }>();
+	const { certificaciones, basePath }: { certificaciones: Certificacion[], basePath: string } = $props();
 </script>
 
 {#snippet statVigencia(title: string, value: string)}
@@ -15,7 +15,7 @@
 
 <ul class="list bg-base-100 rounded-box gap-1 shadow-md">
 	{#each certificaciones as cert}
-		<a href={`/profesionista/certificaciones/${cert.id}`}>
+		<a href={`${basePath}/${cert.id}`}>
 			<li class="list-row my-1 hover:border">
 				<div class="flex flex-col">
 					<h3 class="text-xl font-bold hover:font-bold">{cert.nombre}</h3>
--- src/routes/empleador/+layout.svelte
+++ src/routes/empleador/+layout.svelte
@@ -1,26 +1,46 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { page } from '$app/state';
+	import Search from '$lib/components/search.svelte';
 	import EmpleadorHeader from './empleador-header.svelte';
 
 	const { data, children } = $props();
 	const { currentEmpleador, totalProcesos } = data;
+
+	let searchTerm = $state('');
+
+	function handleSearch() {
+		goto('/empleador/certificaciones?search=' + searchTerm);
+	}
 </script>
 
 <main class="flex flex-col gap-5 p-5 lg:px-20 lg:py-5">
-	<ul class="menu menu-vertical sm:menu-horizontal bg-base-200 rounded-box">
-		<li>
-			<a class={page.url.pathname === '/empleador' ? 'menu-active' : ''} href="/empleador"
-				>Dashboard</a
-			>
-		</li>
-		<li>
-			<a
-				class={page.url.pathname.includes('/empleador/procesos') ? 'menu-active' : ''}
-				href="/empleador/procesos">Procesos</a
-			>
-		</li>
-		<!-- <li><a>Configuración</a></li> -->
-	</ul>
+	<div class="flex flex-col gap-5 md:flex-row md:items-center">
+		<ul class="menu menu-vertical sm:menu-horizontal bg-base-200 rounded-box">
+			<li>
+				<a class={page.url.pathname === '/empleador' ? 'menu-active' : ''} href="/empleador"
+					>Dashboard</a
+				>
+			</li>
+			<li>
+				<a
+					class={page.url.pathname.includes('/empleador/procesos') ? 'menu-active' : ''}
+					href="/empleador/procesos">Procesos</a
+				>
+			</li>
+			<li>
+				<a
+					class={page.url.pathname.includes('/empleador/certificaciones') ? 'menu-active' : ''}
+					href="/empleador/procesos">Buscador</a
+				>
+			</li>
+			<!-- <li><a>Configuración</a></li> -->
+		</ul>
+
+		<div class="grow">
+			<Search bind:searchTerm onsearch={handleSearch} />
+		</div>
+	</div>
 
 	<EmpleadorHeader empleador={currentEmpleador} {totalProcesos} />
 
