241a51aeccd29bdd7a9443f45919630a3775c97b
Angel M <angelmtzdiaz@gmail.com>
2025-03-26 01:21:46 -0600

    feat(routes): impl filters

--- src/lib/components/search.svelte
+++ src/lib/components/search.svelte
@@ -1,34 +1,69 @@
 <script lang="ts">
+	import type { Filter, FilterType } from '$lib/search.svelte';
+	import CrossSvg from './cross-svg.svelte';
+	import FilterSvg from './filter-svg.svelte';
+	import FiltersSelect from './filters-select.svelte';
+	import SearchSvg from './search-svg.svelte';
+
 	let {
 		searchTerm = $bindable(''),
-		onsearch = () => {}
-	}: { searchTerm: string; onsearch?: () => void } = $props();
+		onsearch = () => {},
+		filters = $bindable([])
+	}: {
+		searchTerm: string;
+		onsearch?: () => void;
+		filters: Filter[];
+	} = $props();
+
+	let showFiltersList = $state(false);
+
+	function toggleFilterList() {
+		showFiltersList = !showFiltersList;
+	}
+
+	function handleFilterSelect(filter: Filter) {
+		filters.push(filter);
+
+		showFiltersList = false;
+	}
 </script>
 
-<div class="join">
-	<label class="input">
-		<svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-			><g
-				stroke-linejoin="round"
-				stroke-linecap="round"
-				stroke-width="2.5"
-				fill="none"
-				stroke="currentColor"
-				><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></g
-			></svg
-		>
-		<input
-			type="search"
-			class="grow"
-			placeholder="Buscar Certificaciones..."
-			bind:value={searchTerm}
-		/>
-		<!-- <kbd class="kbd kbd-sm">⌘</kbd>
+<div class="relative">
+	<div class="join">
+		<label class="input">
+			<SearchSvg />
+			<input
+				type="search"
+				class="grow"
+				placeholder="Buscar Certificaciones..."
+				bind:value={searchTerm}
+			/>
+			<!-- <kbd class="kbd kbd-sm">⌘</kbd>
 			<kbd class="kbd kbd-sm">K</kbd> -->
-	</label>
+		</label>
 
-	<div class="indicator">
-		<!-- <span class="indicator-item badge badge-secondary">new</span> -->
-		<button onclick={onsearch} class="btn join-item">Buscar</button>
+		<div class="indicator">
+			<!-- svelte-ignore a11y_consider_explicit_label -->
+			<div class="tooltip" data-tip={showFiltersList ? 'Cerrar Filtros' : 'Abrir Filtros'}>
+				<button class="btn join-item" onclick={toggleFilterList}>
+					{#if showFiltersList}
+						<CrossSvg />
+					{:else}
+						<FilterSvg />
+					{/if}
+				</button>
+			</div>
+		</div>
+
+		<div class="indicator">
+			{#if showFiltersList}
+				<div class="join-item fixed right-10 z-10 mt-12 sm:right-auto sm:m-auto sm:mr-5">
+					<FiltersSelect onSelect={handleFilterSelect} />
+				</div>
+			{:else}
+				<!-- <span class="indicator-item badge badge-secondary">new</span> -->
+				<button onclick={onsearch} class="btn join-item">Buscar</button>
+			{/if}
+		</div>
 	</div>
 </div>
