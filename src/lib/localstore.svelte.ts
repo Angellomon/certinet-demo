7e98616a8300279beb440194f49d94ac058f04c9
Angel M <angelmtzdiaz@gmail.com>
2025-03-30 00:39:36 -0600

    feat(lib): impl validator

--- src/lib/localstore.svelte.ts
+++ src/lib/localstore.svelte.ts
@@ -1,12 +1,16 @@
 import { browser } from '$app/environment';
+import { parse } from 'valibot';
 import { getDemoEmpleador } from './demo-data';
+import { empleadorSchema } from './entities';
 
-export class LocalStore<T> {
+export class LocalObjectStore<T> {
 	value = $state<T>() as T;
 	#key = '';
+	#schema: any | null;
 
-	constructor(key: string, value: T) {
+	constructor(key: string, value: T, schema: any = null) {
 		this.#key = key;
+		this.#schema = schema;
 		this.value = value;
 
 		if (browser) {
@@ -24,16 +28,19 @@ export class LocalStore<T> {
 	}
 
 	deserialize(item: string): T {
-		return JSON.parse(item);
+		const rawData = JSON.parse(item);
+		if (this.#schema) return parse(this.#schema, rawData);
+		else return rawData;
 	}
 }
 
-export function newLocalStore<T>(key: string, value: T) {
-	return new LocalStore(key, value);
+export function newLocalStore<T>(key: string, value: T, schema: any) {
+	return new LocalObjectStore(key, value, schema);
 }
 
 export function newEmpleadorStore() {
-	const store = newLocalStore('empleador', getDemoEmpleador());
+	const demoData = getDemoEmpleador();
+	const store = newLocalStore('empleador', demoData, empleadorSchema);
 
 	if (typeof store.value.fechaEstablecimiento === 'string')
 		store.value.fechaEstablecimiento = new Date(store.value.fechaEstablecimiento);
