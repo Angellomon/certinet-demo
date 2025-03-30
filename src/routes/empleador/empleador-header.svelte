a51c59876f463a99783390d4877fc7ef5d1c7202
Angel M <angelmtzdiaz@gmail.com>
2025-03-30 01:09:49 -0600

    feat(routes): impl empleador context

--- src/routes/empleador/empleador-header.svelte
+++ src/routes/empleador/empleador-header.svelte
@@ -2,22 +2,28 @@
 	import CountStat from '$lib/components/count-stat.svelte';
 	import VerifiedStatus from '$lib/components/verified-status.svelte';
 	import type { Empleador } from '$lib/entities';
-	import { newEmpleadorStore, LocalObjectStore } from '$lib/localstore.svelte';
+	import { LocalObjectStore } from '$lib/localstore.svelte';
 	import { getContext } from 'svelte';
 
-	const { empleador, totalProcesos } = $props<{ empleador: Empleador; totalProcesos: number }>();
+	interface Props {
+		totalProcesos: number;
+	}
 
-	const localEmpleador = getContext<LocalObjectStore<Empleador>>("empleador-store")
+	const { totalProcesos }: Props = $props();
+
+	const empleadorStore: LocalObjectStore<Empleador> = getContext('empleador-store');
+
+	const localEmpleador = getContext<LocalObjectStore<Empleador>>('empleador-store');
 </script>
 
 <div class="flex flex-col items-start justify-between md:flex-row">
 	<div>
 		<h1 class="text-4xl">{localEmpleador.value.razonSocial}</h1>
 		<div class="flex flex-col justify-start gap-10 md:flex-row md:items-center">
-			<div class="link link-primary text-xl italic">{empleador.correo}</div>
+			<div class="link link-primary text-xl italic">{empleadorStore.value.correo}</div>
 
-			<VerifiedStatus verified={empleador.verificado}>
-				{empleador.verificado ? 'Verificado' : 'Verificación Pendiente'}
+			<VerifiedStatus verified={empleadorStore.value.verificado}>
+				{empleadorStore.value.verificado ? 'Verificado' : 'Verificación Pendiente'}
 			</VerifiedStatus>
 		</div>
 	</div>
