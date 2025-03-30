f0ea8c41e98c3a1461199ad037a8157685f39489
Angel M <angelmtzdiaz@gmail.com>
2025-03-30 04:16:09 -0600

    feat: better listing

--- src/routes/empleador/certificaciones/+page.svelte
+++ src/routes/empleador/certificaciones/+page.svelte
@@ -4,14 +4,42 @@
 	import { SvelteSet } from 'svelte/reactivity';
 
 	let selectedIds: Set<string> = $state(new SvelteSet());
+	let totalSelected = $derived(selectedIds.size);
 
 	function handleNewProcess() {
 		goto(`/empleador/certificaciones/nuevo-proceso/?certIds=${Array.from(selectedIds).join(',')}`);
 	}
 </script>
 
+{#snippet stat(title: string, value: string, desc?: string)}
+	<div class="stats mx-2 text-right">
+		<div class="">
+			<div class="stat-title">
+				{title}
+			</div>
+			<div class="stat-value text-2xl">
+				{value}
+			</div>
+			{#if desc}
+				<div class="stat-desc">
+					{desc}
+				</div>
+			{/if}
+		</div>
+	</div>
+{/snippet}
+
 <main class="relative">
-	<h1 class="mb-5 text-xl">Resultados de Búsqueda</h1>
+	<div class="relative">
+		<h1 class=" mb-5 text-xl">Resultados de Búsqueda</h1>
+
+		<p>Seleccione las <u>certificaciones</u> adecuadas</p>
+
+		<div class=" bottom:5 text-right md:absolute md:top-0 md:right-10">
+			{@render stat('Total Seleccionados', `${totalSelected}`)}
+		</div>
+	</div>
+	<div class="divider"></div>
 
 	<SelectCertificaciones bind:selectedIds />
 
--- src/routes/empleador/certificaciones/nuevo-proceso/+page.svelte
+++ src/routes/empleador/certificaciones/nuevo-proceso/+page.svelte
@@ -2,14 +2,26 @@
 	import { goto } from '$app/navigation';
 	import FormPayment from '$lib/components/form-payment.svelte';
 	import ListaCertificaciones from '$lib/components/lista-certificaciones.svelte';
+	import { generateId } from '$lib/demo-data.js';
+	import type { Empleador, ProcesosContacto } from '$lib/entities.js';
+	import type { LocalObjectStore } from '$lib/localstore.svelte.js';
+	import { getContext } from 'svelte';
 
 	const { data } = $props();
 
-	const { certificaciones } = data;
+	const empleador: LocalObjectStore<Empleador> = getContext('empleador-store');
+	const procesos: LocalObjectStore<ProcesosContacto> = getContext('procesos-contacto');
+
+	const { certificaciones: certs } = data;
+
+	let certificaciones = $state(certs);
+
+	let monto = $derived(certificaciones.length * (empleador.value.precioCertificacion?.unidad || 1));
 
 	function onSubmit() {
-		const idsCerts = certificaciones.map((c) => c.id).join(',');
-		goto(`/empleador/procesos?idsCerts=${idsCerts}`);
+		// const idsCerts = certificaciones.map((c) => c.id).join(',');
+
+		goto(`/empleador/procesos`);
 	}
 </script>
 
@@ -25,11 +37,13 @@
 	<div class="grid grid-cols-1 md:grid-cols-2">
 		<div class="mx-3">
 			<h2 class="text-right text-xl">Certificaciones Seleccionadas</h2>
-			<ListaCertificaciones {certificaciones} />
+			<ListaCertificaciones bind:certificaciones />
 		</div>
 		<div class="mx-3 flex flex-col gap-5">
 			<h2 class="text-right text-xl">Info Pago</h2>
-			<FormPayment {onSubmit} />
+			<FormPayment {monto} {onSubmit} />
+
+			<button class="btn m1l-5" onclick={() => history.back()}>Cancelar</button>
 		</div>
 	</div>
 </main>
