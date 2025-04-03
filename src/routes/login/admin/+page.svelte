d59c6f6c1dd932e28fb3e8065ff4c701fe3e3b85
Angel M <angelmtzdiaz@gmail.com>
2025-04-03 01:56:13 -0600

    feat: impl admin login route

---
+++ src/routes/login/admin/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import EmailInput from '../email-input.svelte';
+	import PasswordInput from '../password-input.svelte';
+
+	let email = $state('');
+	let password = $state('');
+</script>
+
+<main class="my-30 flex flex-col items-center gap-10">
+	<h1 class="text-3xl">Login Admin</h1>
+
+	<div class="flex max-w-1/2 flex-col items-center gap-3">
+		<EmailInput bind:email />
+
+		<PasswordInput bind:password />
+
+		<a class="btn btn-neutral btn-block" href="/admin">Login</a>
+	</div>
+</main>
