067613f7e1f48167fba7d5f2fc29110f5e58b7e6
Angel M <angelmtzdiaz@gmail.com>
2025-04-03 02:48:48 -0600

    feat: props typing + impl monto

--- src/lib/components/count-stat.svelte
+++ src/lib/components/count-stat.svelte
@@ -1,13 +1,24 @@
 <script lang="ts">
 	import { formatNumber } from '$lib/utils';
 
-	const { count = 0, description = '', title = 'Total' } = $props();
+	interface Props {
+		count: number,
+		description?: string,
+		title: string,
+		isMoney?: boolean
+	}
+
+	const { count = 0, description = '', title = 'Total', isMoney = false }: Props = $props();
 </script>
 
 <div class="stats shadow">
 	<div class="stat">
 		<div class="stat-title">{title}</div>
+		{#if isMoney}
+		<div class="stat-value">$ {formatNumber(count)}</div>
+		{:else}
 		<div class="stat-value">{formatNumber(count)}</div>
+		{/if}
 
 		{#if description}
 			<div class="stat-desc">{description}</div>
