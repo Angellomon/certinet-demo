60de940bdef73902f6229b1b7a13dd18740696df
Angel M <angelmtzdiaz@gmail.com>
2025-04-05 13:28:44 -0600

    feat(lib): impl general menu component

---
+++ src/lib/components/menu-seccion.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	interface Props {
+		data: {
+			seccion: string;
+			title: string;
+		}[];
+		current: string;
+	}
+
+	let { current = $bindable(), data }: Props = $props();
+
+	function setSeccion(s: string) {
+		current = s;
+	}
+</script>
+
+<ul class="menu menu-vertical bg-base-200 rounded-box w-56">
+	{#each data as _data}
+		<li>
+			<button
+				class:menu-active={current == _data.seccion}
+				onclick={() => setSeccion(_data.seccion)}
+			>
+				{_data.title}
+			</button>
+		</li>
+	{/each}
+</ul>
