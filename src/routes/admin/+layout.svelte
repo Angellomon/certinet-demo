04030045e91a3468f24fb41b7600c4caaa5f55e7
Angel M <angelmtzdiaz@gmail.com>
2025-04-05 13:30:29 -0600

    feat: impl admin config menu store

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,15 +1,31 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { page } from '$app/state';
+	import SettingsSvg from '$lib/components/settings-svg.svelte';
+	import { newSeccionAdminConfigStore } from '$lib/localstore.svelte';
+	import { setContext } from 'svelte';
 	import AdminHeader from './admin-header.svelte';
 
 	const { children } = $props();
+
+	const seccionAdminConfigStore = newSeccionAdminConfigStore();
+	setContext('seccion-admin-config-store', seccionAdminConfigStore);
+
+	function handleMenuClick() {
+		seccionAdminConfigStore.value = 'datos';
+	}
 </script>
 
 <main class="flex flex-col gap-5 p-5 lg:px-20 lg:py-5">
 	<AdminHeader />
 
 	<div class="flex-row-flex-wrap flex flex-col items-center justify-between sm:flex-row">
-		<ul class="menu menu-horizontal bg-base-200 rounded-box justify-around">
+		<!-- svelte-ignore a11y_click_events_have_key_events -->
+		<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+		<ul
+			class="menu menu-horizontal bg-base-200 rounded-box justify-around"
+			onclick={handleMenuClick}
+		>
 			<li>
 				<a class:menu-active={page.url.pathname === '/admin'} href="/admin"> Dashboard </a>
 			</li>
@@ -46,6 +62,14 @@
 			</li>
 			<!-- <li><a>Configuración</a></li> -->
 		</ul>
+
+		<button
+			class="btn"
+			class:btn-neutral={page.url.pathname.includes('/config')}
+			onclick={() => goto('/admin/config')}
+		>
+			Configuración <SettingsSvg />
+		</button>
 	</div>
 	{@render children()}
 </main>
