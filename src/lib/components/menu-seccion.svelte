0d7538b46b1c548cc9153b29cb7d8571eeb03e80
Angel M <angelmtzdiaz@gmail.com>
2025-04-08 14:47:08 -0600

    feat(lib): impl menu component url mode

--- src/lib/components/menu-seccion.svelte
+++ src/lib/components/menu-seccion.svelte
@@ -3,26 +3,38 @@
 		data: {
 			seccion: string;
 			title: string;
+			url?: string;
 		}[];
 		current: string;
+		urlMode?: boolean;
 	}
 
-	let { current = $bindable(), data }: Props = $props();
+	let { current = $bindable(), data, urlMode = false }: Props = $props();
 
 	function setSeccion(s: string) {
 		current = s;
 	}
 </script>
 
-<ul class="menu menu-vertical bg-base-200 rounded-box w-56">
+<ul class="menu menu-horizontal sm:menu-vertical bg-base-200 rounded-box w-full sm:w-56">
 	{#each data as _data}
 		<li>
-			<button
-				class:menu-active={current == _data.seccion}
-				onclick={() => setSeccion(_data.seccion)}
-			>
-				{_data.title}
-			</button>
+			{#if urlMode &&_data.url }
+				<a
+					href={_data.url}
+					class:menu-active={current == _data.seccion}
+					onclick={() => setSeccion(_data.seccion)}
+				>
+					{_data.title}
+				</a>
+			{:else}
+				<button
+					class:menu-active={current == _data.seccion}
+					onclick={() => setSeccion(_data.seccion)}
+				>
+					{_data.title}
+				</button>
+			{/if}
 		</li>
 	{/each}
 </ul>
