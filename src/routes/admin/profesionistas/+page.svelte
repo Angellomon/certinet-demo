80b66a5858a3e81ea049676bc49b13a53bfcca92
Angellomon <angelmtzdiaz@gmail.com>
2025-04-15 15:29:56 -0600

    rename title

--- src/routes/admin/profesionistas/+page.svelte
+++ src/routes/admin/profesionistas/+page.svelte
@@ -2,9 +2,8 @@
 	import TableProfesionistas from '$lib/components/table-profesionistas.svelte';
 </script>
 
-<h1 class="text-2xl">General</h1>
+<h1 class="text-2xl">Profesionistas</h1>
 
-<main class="flex flex-col gap-5 overflow-scroll">
-	<h2 class="text-xl">Recientes</h2>
-	<TableProfesionistas limit={10} />
+<main class="flex flex-col gap-5">
+	<TableProfesionistas baseURL="/admin/profesionistas" showActions showVerifyActions />
 </main>
