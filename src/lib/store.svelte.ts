db52dcb042fc8dcb1a308b1e21ac4c224955feb0
Angellomon <angelmtzdiaz@gmail.com>
2025-04-17 03:06:49 -0600

    impl store class

---
+++ src/lib/store.svelte.ts
@@ -0,0 +1,26 @@
+import { breadcrumbsDataSchema, type BreadcrumbsData } from './entities';
+import { parse } from 'valibot';
+
+export class Store<T> {
+	value = $state<T>() as T;
+	#schema: any | null = null;
+
+	constructor(value: T, schema: any = null) {
+		this.#schema = schema;
+		this.value = value;
+	}
+
+	serialize(value: T): string {
+		return JSON.stringify(value);
+	}
+
+	deserialize(item: string): T {
+		const rawData = JSON.parse(item);
+		if (this.#schema) return parse(this.#schema, rawData);
+		else return rawData;
+	}
+}
+
+export function newbreadcrumbsStore(data: BreadcrumbsData) {
+	return new Store(data, breadcrumbsDataSchema)
+}
