1941e345fd8713b60369d82f5c77d25d82bde1b3
Angellomon <angelmtzdiaz@gmail.com>
2025-04-17 03:06:59 -0600

    impl breadcrumbs component

---
+++ src/lib/components/breadcrumbs.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { BreadcrumbsData } from "$lib/entities";
+
+	interface Props {
+		data: BreadcrumbsData;
+	}
+	const { data }: Props = $props();
+</script>
+
+<ul class="breadcrumbs text-sm px-2">
+	{#each data as item}
+		<li>
+			{#if item.url}
+				<a href={item.url}>
+					{item.title}
+				</a>
+			{:else}
+				{item.title}
+			{/if}
+		</li>
+	{/each}
+</ul>
