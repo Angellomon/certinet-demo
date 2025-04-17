074492df270835c43c8e0163cf9dbf8ca78a61fe
Angellomon <angelmtzdiaz@gmail.com>
2025-04-17 03:07:45 -0600

    impl menu context + Menu component

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -4,72 +4,97 @@
 	import SettingsSvg from '$lib/components/settings-svg.svelte';
 	import {
 		newSeccionAdminConfigStore,
-		newSeccionAdminProfesionistasStore
+		newSeccionAdminProfesionistasStore,
+		newSeccionAdminProfesionistaStore,
+		newSeccionAdminStore
 	} from '$lib/localstore.svelte';
 	import { setContext } from 'svelte';
 	import AdminHeader from './admin-header.svelte';
+	import {
+		setBreadCrumbsContext,
+		setSeccionAdminContext,
+		setSeccionAdminProfesionistaContext
+	} from '$lib/context.svelte';
+	import MenuSeccion from '$lib/components/menu-seccion.svelte';
+	import type { BreadcrumbsData, SeccionAdmin, SeccionAdminProfesionistas } from '$lib/entities';
+	import Breadcrumbs from '$lib/components/breadcrumbs.svelte';
+	import { newbreadcrumbsStore } from '$lib/store.svelte';
 
 	const { children } = $props();
+
+	const seccionAdminStore = newSeccionAdminStore();
+	setSeccionAdminContext(seccionAdminStore);
 
 	const seccionAdminConfigStore = newSeccionAdminConfigStore();
 	setContext('seccion-admin-config-store', seccionAdminConfigStore);
 
-	const seccionAdminProfesionistasStore = newSeccionAdminProfesionistasStore();
-	setContext('seccion-admin-profesionistas-store', seccionAdminProfesionistasStore);
+	const seccionAdminProfesionistaStore = newSeccionAdminProfesionistaStore();
+	setSeccionAdminProfesionistaContext(seccionAdminProfesionistaStore);
+
+	const breadcrumbsAdmin = newbreadcrumbsStore([
+		{
+			title: 'Admin',
+			url: '/admin'
+		}
+	]);
+	setBreadCrumbsContext(breadcrumbsAdmin);
+
+	const menuData: {
+		url?: string;
+		title: string;
+		seccion: SeccionAdmin;
+	}[] = [
+		{
+			seccion: 'dashboard',
+			title: 'Dashboard',
+			url: '/admin'
+		},
+		{
+			seccion: 'profesionistas',
+			title: 'Profesionistas',
+			url: '/admin/profesionistas'
+		},
+		{
+			seccion: 'empleadores',
+			title: 'Empleadores',
+			url: '/admin/empleadores'
+		},
+		{
+			seccion: 'certificaciones',
+			title: 'Certificaciones',
+			url: '/admin/certificaciones'
+		},
+		{
+			seccion: 'procesos',
+			title: 'Procesos de Contacto',
+			url: '/admin/procesos'
+		},
+		{
+			seccion: 'compras',
+			title: 'Compras',
+			url: '/admin/compras'
+		}
+	];
 
 	function handleMenuClick() {
 		seccionAdminConfigStore.value = 'datos';
-		seccionAdminProfesionistasStore.value = 'dashboard';
+		seccionAdminProfesionistaStore.value = 'datos';
 	}
 </script>
 
-<main class="flex flex-col gap-5 p-5 lg:px-20 lg:py-5">
+<main class="flex flex-col gap-1 p-5 lg:px-20 lg:py-5">
 	<AdminHeader />
 
 	<div class="flex-row-flex-wrap flex flex-col items-center justify-between sm:flex-row">
 		<!-- svelte-ignore a11y_click_events_have_key_events -->
 		<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
-		<ul
-			class="menu menu-horizontal bg-base-200 rounded-box justify-around"
-			onclick={handleMenuClick}
-		>
-			<li>
-				<a class:menu-active={page.url.pathname === '/admin'} href="/admin"> Dashboard </a>
-			</li>
-			<li>
-				<a
-					class:menu-active={page.url.pathname.includes('/profesionistas')}
-					href="/admin/profesionistas"
-				>
-					Profesionistas
-				</a>
-			</li>
-			<li>
-				<a class:menu-active={page.url.pathname.includes('/empleadores')} href="/admin/empleadores">
-					Empleadores
-				</a>
-			</li>
-			<li>
-				<a class:menu-active={page.url.pathname.includes('/procesos')} href="/admin/procesos">
-					Procesos
-				</a>
-			</li>
-			<li>
-				<a
-					class:menu-active={page.url.pathname.includes('/certificaciones')}
-					href="/admin/certificaciones"
-				>
-					Certificaciones
-				</a>
-			</li>
-			<li>
-				<a class:menu-active={page.url.pathname.includes('/compras')} href="/admin/compras">
-					Compras
-				</a>
-			</li>
-			<!-- <li><a>Configuración</a></li> -->
-		</ul>
-
+		<MenuSeccion
+			data={menuData}
+			current={seccionAdminStore.value}
+			onMenuClick={handleMenuClick}
+			urlMode
+			size="lg"
+		/>
 		<button
 			class="btn"
 			class:btn-neutral={page.url.pathname.includes('/config')}
@@ -78,6 +103,9 @@
 			Configuración <SettingsSvg />
 		</button>
 	</div>
+
+	<Breadcrumbs data={breadcrumbsAdmin.value} />
+
 	<div class="overflow-x-auto">
 		{@render children()}
 	</div>
