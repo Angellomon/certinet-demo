8dede142bcfae1fc1320eada5bd94fe71925abac
Angellomon <angelmtzdiaz@gmail.com>
2025-04-17 03:07:13 -0600

    impl more menu options

--- src/lib/components/menu-seccion.svelte
+++ src/lib/components/menu-seccion.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	type Size = 'sm' | 'md' | 'lg' | 'xl';
 	interface Props {
 		data: {
 			seccion: string;
@@ -7,16 +8,41 @@
 		}[];
 		current: string;
 		urlMode?: boolean;
+		vertical?: boolean;
+		onMenuClick?: () => void;
+		size?: Size;
 	}
 
-	let { current = $bindable(), data, urlMode = false }: Props = $props();
+	let {
+		current = $bindable(),
+		data,
+		urlMode = false,
+		vertical = false,
+		onMenuClick = () => {},
+		size = 'sm'
+	}: Props = $props();
 
 	function setSeccion(s: string) {
 		current = s;
 	}
+
+	function handleMenuClick() {
+		onMenuClick();
+	}
 </script>
 
-<ul class="menu menu-horizontal sm:menu-vertical bg-base-200 rounded-box w-full sm:w-56">
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+<ul
+	class="menu menu-horizontal bg-base-200 rounded-box w-full"
+	class:lg:menu-vertical={vertical}
+	class:md:w-56={vertical}
+	class:menu-sm={size === 'sm'}
+	class:menu-md={size === 'md'}
+	class:menu-lg={size === 'lg'}
+	class:menu-xl={size === 'xl'}
+	onclick={handleMenuClick}
+>
 	{#each data as _data}
 		<li>
 			{#if urlMode && _data.url}
