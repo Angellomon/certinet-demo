64abac6a52ae6d6705749efa1cdfee21b4bcc7cf
Angellomon <angelmtzdiaz@gmail.com>
2025-04-17 16:49:38 -0600

    impl certs table

---
+++ src/routes/admin/profesionistas/[idProfesionista]/certificaciones/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import TableCertificaciones from '$lib/components/table-certificaciones.svelte';
+
+	const { idProfesionista } = page.params;
+</script>
+
+<main class="flex flex-col gap-5">
+	<TableCertificaciones {idProfesionista} showVerifyActions />
+</main>
