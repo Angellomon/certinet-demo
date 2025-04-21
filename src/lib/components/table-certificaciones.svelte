6e952ddb4ef5774330f28be8440f919e64bade52
Angellomon <angelmtzdiaz@gmail.com>
2025-04-21 17:12:26 -0600

    fix: toggle input alignment

--- src/lib/components/table-certificaciones.svelte
+++ src/lib/components/table-certificaciones.svelte
@@ -74,36 +74,38 @@
 							: `${cert.vigencia} años`}
 				</td>
 				<td>
-					{@render badgeVerificado(cert.verificado)}
-					{#if showVerifyActions}
-						<label class="toggle text-base-content my-2 self-center">
-							<input type="checkbox" bind:checked={cert.verificado} />
-							<svg
-								aria-label="disabled"
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 24 24"
-								fill="none"
-								stroke="currentColor"
-								stroke-width="4"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-							>
-								<path d="M18 6 6 18" />
-								<path d="m6 6 12 12" />
-							</svg>
-							<svg aria-label="enabled" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-								<g
-									stroke-linejoin="round"
-									stroke-linecap="round"
-									stroke-width="4"
+					<div class="join items-center gap-2">
+						{@render badgeVerificado(cert.verificado)}
+						{#if showVerifyActions}
+							<label class="toggle text-base-content my-2 self-center">
+								<input type="checkbox" bind:checked={cert.verificado} />
+								<svg
+									aria-label="disabled"
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 24 24"
 									fill="none"
 									stroke="currentColor"
+									stroke-width="4"
+									stroke-linecap="round"
+									stroke-linejoin="round"
 								>
-									<path d="M20 6 9 17l-5-5"></path>
-								</g>
-							</svg>
-						</label>
-					{/if}
+									<path d="M18 6 6 18" />
+									<path d="m6 6 12 12" />
+								</svg>
+								<svg aria-label="enabled" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+									<g
+										stroke-linejoin="round"
+										stroke-linecap="round"
+										stroke-width="4"
+										fill="none"
+										stroke="currentColor"
+									>
+										<path d="M20 6 9 17l-5-5"></path>
+									</g>
+								</svg>
+							</label>
+						{/if}
+					</div>
 				</td>
 			</tr>
 		{/each}
