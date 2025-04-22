cf68ce1ea89e8d5c11c82c0c683693ef25b8894d
Angellomon <angelmtzdiaz@gmail.com>
2025-04-22 01:32:17 -0600

    feat: impl search input

---
+++ src/lib/components/search-input.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { MagnifyingGlass } from 'phosphor-svelte';
+
+	interface Props {
+		searchTerm?: string;
+		placeholder?: string;
+		onSearch: (searchTerm: string) => void;
+	}
+
+	let { searchTerm = $bindable(''), onSearch, placeholder = 'Buscar...' }: Props = $props();
+
+	function handleSearch() {
+		onSearch(searchTerm);
+	}
+</script>
+
+<div class="join w-full">
+	<label class="input w-full">
+		<MagnifyingGlass />
+		<input bind:value={searchTerm} {placeholder} type="search" required />
+	</label>
+	<!-- svelte-ignore a11y_click_events_have_key_events -->
+	<!-- svelte-ignore a11y_no_static_element_interactions -->
+	<buttton class="btn btn-neutral" onclick={handleSearch}> Buscar </buttton>
+</div>
