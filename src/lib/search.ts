c190eb6ba605d8887a5cafd43c0432d620b3d499
Angellomon <angelmtzdiaz@gmail.com>
2025-04-21 23:51:02 -0600

    feat: impl profesionistas index

---
+++ src/lib/search.ts
@@ -0,0 +1,86 @@
+import { Index } from 'flexsearch';
+import type { Curriculum, Profesionista, Profesionistas } from './entities';
+
+function escapeRegExp(text: string) {
+	return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+}
+
+abstract class SearchIndex<T> {
+	protected index: Index;
+
+	constructor() {
+		this.index = new Index({ tokenize: 'forward' });
+	}
+
+	abstract addToIndex(...data: T[]): void;
+	abstract search(searchTerm: string): string[];
+	abstract updateIndex(data: T): void;
+	abstract removeFromIndex(id: string): void;
+}
+
+export class ProfesionistasIndex extends SearchIndex<Profesionista> {
+	constructor() {
+		super();
+	}
+
+	search(searchTerm: string): string[] {
+		const text = escapeRegExp(searchTerm);
+		return this.index.search(text) as any;
+	}
+
+	convertCurriculum(data: Curriculum[]) {
+		let result = '';
+
+		const tags = new Set();
+
+		for (let curr of data) {
+			for (let tech of curr.tech) {
+				tags.add(tech);
+			}
+		}
+
+		result = result.concat(Array.from(tags).join(','));
+
+		return result;
+	}
+
+	convertProfesionista(data: Profesionista) {
+		let result = '';
+
+		result = result.concat(`${data.nombre},`);
+		result = result.concat(`${data.apellidos.replaceAll(' ', '')},`);
+		result = result.concat(`${data.profesion},`);
+		result = result.concat(`${data.correo},`);
+		result = result.concat(this.convertCurriculum(data.trayectoria.laboral));
+		result = result.concat(this.convertCurriculum(data.trayectoria.proyectos));
+
+		return result;
+	}
+
+	addToIndex(...data: Profesionistas): void {
+		for (let profesionista of data) {
+			const profesionistaStr = this.convertProfesionista(profesionista);
+			this.index.add(profesionista.id, profesionistaStr);
+		}
+	}
+
+	updateIndex(data: Profesionista) {
+		if (this.index.contain(data.id)) this.index.update(data.id, this.convertProfesionista(data));
+		else this.addToIndex(data);
+	}
+
+	removeFromIndex(id: string): void {
+		if (this.index.contain(id)) this.index.remove(id);
+	}
+}
+
+
+export function newProfesionistasIndex(...data: Profesionistas) {
+	if (!data) return new ProfesionistasIndex();
+
+	const index = new ProfesionistasIndex();
+
+	index.addToIndex(...data);
+
+	return index;
+}
