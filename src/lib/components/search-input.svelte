be4569b72b7082ab44730c145987c4c22492b75e
Angellomon <angelmtzdiaz@gmail.com>
2025-04-22 03:47:51 -0600

    support key enter

--- src/lib/components/search-input.svelte
+++ src/lib/components/search-input.svelte
@@ -12,12 +12,16 @@
 	function handleSearch() {
 		onSearch(searchTerm);
 	}
+
+	function handleKeyDown(e: KeyboardEvent) {
+		if (e.code === 'Enter') handleSearch();
+	}
 </script>
 
 <div class="join w-full">
 	<label class="input w-full">
 		<MagnifyingGlass />
-		<input bind:value={searchTerm} {placeholder} type="search" required />
+		<input bind:value={searchTerm} {placeholder} type="search" required onkeydown={handleKeyDown} />
 	</label>
 	<!-- svelte-ignore a11y_click_events_have_key_events -->
 	<!-- svelte-ignore a11y_no_static_element_interactions -->
