3286e3b26fe2a55af8ee95c230f182694db7c732
Angellomon <angelmtzdiaz@gmail.com>
2025-04-26 03:13:12 -0600

    feat: impl table certificaciones on admin

---
+++ src/routes/admin/certificaciones/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import TableCertificaciones from "$lib/components/tables/table-certificaciones.svelte";
+
+</script>
+
+<h1 class="pb-5 text-2xl">Empleadores</h1>
+
+<main class="flex flex-col gap-5">
+	<TableCertificaciones showSearch baseURL="/admin/certificaciones" />
+</main>
