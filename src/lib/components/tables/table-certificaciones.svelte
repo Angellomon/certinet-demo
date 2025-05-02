c882f691ee9505a43681ad3c4a91afddb2eefc73
Angellomon <angelmtzdiaz@gmail.com>
2025-05-02 15:45:04 -0600

    styling

--- src/lib/components/tables/table-certificaciones.svelte
+++ src/lib/components/tables/table-certificaciones.svelte
@@ -376,7 +376,7 @@
 {/if}
 
 <div class="rounded-box border-base-content/5 bg-base-100 overflow-x-auto border">
-	<table class="table-sm table">
+	<table class="table-xs md:table-sm lg:table-md 2xl:table-lg table">
 		{@render header()}
 		{@render body()}
 	</table>
