3963e109855fa6532482a0fd1d01f2f1eb0e46fc
Angellomon <angelmtzdiaz@gmail.com>
2025-05-06 01:02:39 -0600

    impl empleador compras page

---
+++ src/routes/admin/empleadores/[idEmpleador]/compras/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import TableCompras from '$lib/components/tables/table-compras.svelte';
+
+	const { idEmpleador } = page.params;
+</script>
+
+<main class="flex flex-col gap-5">
+	<TableCompras {idEmpleador} />
+</main>
