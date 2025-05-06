41eef2b9e71d6c6ce482ecf43aff7d1a1f7acbe0
Angellomon <angelmtzdiaz@gmail.com>
2025-05-06 01:02:49 -0600

    impl empleador procesos page

---
+++ src/routes/admin/empleadores/[idEmpleador]/procesos/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from "$app/state";
+	import TableProcesosContacto from "$lib/components/tables/table-procesos-contacto.svelte";
+
+  const { idEmpleador } = page.params
+
+</script>
+
+<main class="flex flex-col gap-5">
+  <TableProcesosContacto {idEmpleador} />
+</main>
