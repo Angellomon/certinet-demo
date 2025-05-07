7ac7f37029dcc76f418d077c0d61aa3c8e67c528
Angellomon <angelmtzdiaz@gmail.com>
2025-05-07 03:42:47 -0600

    impl url mode on procesos table

--- src/routes/admin/procesos/+page.svelte
+++ src/routes/admin/procesos/+page.svelte
@@ -5,5 +5,5 @@
 <h1 class="pb-5 text-2xl">Procesos Contacto</h1>
 
 <main class="flex flex-col gap-5">
-	<TableProcesos showSearch />
+	<TableProcesos baseURL="/admin/procesos" showSearch />
 </main>
