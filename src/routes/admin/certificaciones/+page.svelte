a8987f36d0f51d3599d060c76421e0fa4d7ffdfb
Angellomon <angelmtzdiaz@gmail.com>
2025-05-07 18:33:20 -0600

    fix title

--- src/routes/admin/certificaciones/+page.svelte
+++ src/routes/admin/certificaciones/+page.svelte
@@ -3,7 +3,7 @@
 
 </script>
 
-<h1 class="pb-5 text-2xl">Empleadores</h1>
+<h1 class="pb-5 text-2xl">Certificaciones</h1>
 
 <main class="flex flex-col gap-5">
 	<TableCertificaciones showSearch baseURL="/admin/certificaciones" />
